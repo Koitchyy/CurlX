24efbe2f632b99192cd746b179db304872f4ce89
Chandramouli Gade <chandramouli085@gmail.com>
2026-03-08 09:56:07 -0700

    Added competition code

--- Competition_Code/Competition_Code.ino
+++ Competition_Code/Competition_Code.ino
@@ -20,18 +20,20 @@ const int lineSensorPin = 18;
 const float thresholdAmplitude = 2.1; 
 const int lineThreshold = 50;         
 const unsigned long lineSampleWindow = 100;
-const unsigned long rightDuration = 500;
-const unsigned long returnDuration = 3000; 
-const unsigned long reverseDuration = 4000;
+const unsigned long rightDuration = 950;
+const unsigned long fastRightTime = 100;
+const unsigned long returnDuration = 2000; 
+const unsigned long reverseDuration = 2000;
+const unsigned long reverseAgainDuration = 500;
 const unsigned long reloadDuration = 5000; // 5 Seconds to reload
-const int shootSpeed = 130;
-const int stepsTo90 = 888;           
+int shootSpeed = 145;
+const int stepsTo90 = 825;           
 const unsigned long stepDelayMicros = 1000; 
-const unsigned long stepInterval = 8000; 
+const unsigned long stepInterval = 9000; 
 const unsigned long finalWait = 1500; 
 
 // State Management
-enum State { SEARCHING, SHIFTING_RIGHT, SHIFTING_MORE_RIGHT, REVERSING, SHOOTING, RETURNING, RELOAD, FINISHED };
+enum State { SEARCHING, REVERSING, FAST_SHIFTING_RIGHT, SHIFTING_RIGHT, SHIFTING_MORE_RIGHT, REVERSING_AGAIN, SHOOTING, RETURNING, RELOAD, FINISHED };
 State currentState = SEARCHING;
 
 enum ShootSubState { WAIT1, TURN1, WAIT2, TURN2, WAIT3, TURN3, FINAL_DELAY };
@@ -62,6 +64,7 @@ void setup() {
     robot.begin(115, 100, 100, 100);
     Serial.println("System Initialized. Starting Run 1...");
     robot.turnRight();
+    //robot.shiftRight();
     stateStartTime = millis();
 }
 
@@ -92,6 +95,14 @@ void loop() {
         case REVERSING:
             if (isTimeElapsed(stateStartTime, reverseDuration)) {
                 robot.stop();
+                startNewState(FAST_SHIFTING_RIGHT);
+                robot.shiftRight();
+            }
+            break;
+        
+         case FAST_SHIFTING_RIGHT:
+            if (isTimeElapsed(stateStartTime, fastRightTime)) {
+                //robot.stop();
                 startNewState(SHIFTING_RIGHT);
                 robot.shiftRight();
             }
@@ -99,7 +110,7 @@ void loop() {
 
         case SHIFTING_RIGHT:
             if (isLineDetected()) {
-                robot.stop();
+                //robot.stop();
                 startNewState(SHIFTING_MORE_RIGHT);
                 robot.shiftRight();
             }
@@ -107,6 +118,14 @@ void loop() {
 
         case SHIFTING_MORE_RIGHT:
             if (isTimeElapsed(stateStartTime, rightDuration)) {
+                robot.stop();
+                startNewState(REVERSING_AGAIN);
+                robot.backward(); 
+            }
+            break;
+
+        case REVERSING_AGAIN:
+            if (isTimeElapsed(stateStartTime, reverseAgainDuration)) {
                 robot.stop();
                 startNewState(SHOOTING);
                 shootPhase = WAIT1;
@@ -127,14 +146,19 @@ void loop() {
             }
             break;
 
+
         case RELOAD:
             if (isTimeElapsed(stateStartTime, reloadDuration)) {
                 cycleCount++;
                 if (cycleCount < MAX_CYCLES) {
                     Serial.print("Reload done. Starting Run "); Serial.println(cycleCount + 1);
-                    startNewState(SEARCHING);
-                    robot.turnRight();
-                } else {
+                    if (cycleCount == 1 || cycleCount == 2){
+                        shootSpeed = 138;
+                    }
+                    startNewState(FAST_SHIFTING_RIGHT);
+                    robot.shiftRight();
+                } 
+                else {
                     Serial.println("All cycles complete.");
                     startNewState(FINISHED);
                 }
@@ -152,10 +176,16 @@ void startNewState(State newState) {
     stateStartTime = millis();
     currentState = newState;
     if (newState == SHIFTING_RIGHT || newState == SHIFTING_MORE_RIGHT) {
-        robot.begin(78, 71, 88, 95);
+        robot.begin(72, 72, 81, 103);
         noInterrupts(); pulseCount = 0; lastSavedPulseCount = 125; interrupts();
         lastLineCheckTime = millis();
-    } else {
+    } else if(newState == FAST_SHIFTING_RIGHT) {
+        robot.begin(80, 80, 90, 115);
+    } 
+    else if(newState == REVERSING || newState == REVERSING_AGAIN){
+        robot.begin(115, 100, 100, 115);
+    }
+    else {
         robot.begin(115, 100, 100, 100);
     }
 }
